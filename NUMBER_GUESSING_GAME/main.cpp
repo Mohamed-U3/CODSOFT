691b854b4d06922ed47b034c854d3fabf1d7ef39
Mohamed Yousry <mmyrali1@gmail.com>
2024-01-04 22:00:13 +0200

    - making infinite loop that will continue till the user 	find the correct answer. - in the loop we check if the anwser in close to the random 	number or not and give hint to the user. - finally the project is tested and worked as expected.

--- NUMBER_GUESSING_GAME/main.cpp
+++ NUMBER_GUESSING_GAME/main.cpp
@@ -7,13 +7,42 @@ using namespace std;
 int main()
 {
     srand((unsigned) time(NULL));
-    for(int x=0; x<30; x++)
+    int random = 1 + (rand() % 80); //Generating Random number from 0 to 79 then adding 1 to the result to shift the range from 1 to 80
+    int answer;
+    int rounds = 0;
+
+
+    // Output text
+    cout << " \n\t\t\t Welcome to Guessing the Random Number Game\n";
+    cout << "Now .. I Generated random number from 1 >> 80 try to guess the number as much as U want.\n";
+
+    while (answer != random)
     {
-        int random = 1 + (rand() % 80); //Generating Random number from 0 to 79 then adding 1 to the result to shift the range from 1 to 80
+        cout << "Please enter your guess: ";
+        cin >> answer;
+        rounds++;
 
-        cout<<random<<"\n";
-
+        if (answer > random+10)
+        {
+            cout << "the answer is too high! Try again.\n";
+        }
+        else if (answer > random)
+        {
+            cout << "the answer is little higher then the random number! Try again.\n";
+        }
+        else if (answer+10 < random)
+        {
+            cout << "the answer is too low! Try again.\n";
+        }
+        else if (answer < random)
+        {
+            cout << "the answer is little lower then the random number! Try again.\n";
+        }
+        else
+        {
+            cout << "Congratulations! You guessed the number in " << rounds << " attempts!\n";
+            break;
+        }
     }
-
     return 0;
 }
